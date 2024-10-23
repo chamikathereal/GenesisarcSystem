e8d988a512968fce153f90c6aaee83702cc6adbf
Chamika Gayashan <118957556+chamikathereal@users.noreply.github.com>
2024-10-23 15:32:27 +0530

    Update GRN.java

--- src/lk/genesisarcsystem/gui/GRN.java
+++ src/lk/genesisarcsystem/gui/GRN.java
@@ -14,8 +14,7 @@ import javax.swing.table.DefaultTableModel;
 import lk.genesisarcsystem.model.GRNItem;
 
 public class GRN extends javax.swing.JFrame {
-//this is a hashmap
-    //dev by chamika
+
     //this is my project
 
     HashMap<String, GRNItem> grnItemMap = new HashMap<>();
